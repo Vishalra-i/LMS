059d3cbd38ac26d00515b9e6ae5c82d0609de2bc
Vishalra-i <vishalrai0392@gmail.com>
2024-10-05 16:11:55 +0530

    timeout hanler

--- server/src/utils/asynchandler.js
+++ server/src/utils/asynchandler.js
@@ -1,9 +1,21 @@
-export default function asyncHandler(requestHandle) {
+export default function asyncHandler(requestHandle, timeout = 9500) {  // Timeout in milliseconds (9.5 seconds)
     return async (req, res, next) => {
-        Promise.resolve(requestHandle(req, res, next))
+        // Create a promise that rejects after the specified timeout
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject(new Error('Request timed out. Please try again later.'));
+            }, timeout);
+        });
+
+        // Handle the request and catch errors, including the timeout
+        Promise.race([Promise.resolve(requestHandle(req, res, next)), timeoutPromise])
             .catch(err => {
-                console.log(err);
-                next(err);
+                console.error(err);
+                if (err.message === 'Request timed out. Please try again later.') {
+                    res.status(504).json({ message: err.message });
+                } else {
+                    next(err);
+                }
             });
     };
 }
